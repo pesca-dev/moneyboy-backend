c05bf17956a94e1c5287b8551541df846354ff1e
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-05-23 15:05:16 +0200

    Add helmet for securing API

--- src/server/init.ts
+++ src/server/init.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { json } from "body-parser";
+import helmet from "helmet";
 
 /**
  * Init a new server with some default middleware and return it.
@@ -8,6 +9,7 @@ import { json } from "body-parser";
  */
 export function initServer(): express.Application {
     const server = express();
+    server.use(helmet());
 
     server.use(json());
 
