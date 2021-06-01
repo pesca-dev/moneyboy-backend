0b0af6e4c404ceea85c65accb66e10aaf80e38e6
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-06-01 19:44:29 +0200

    Return user information from profile route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Req } from "@nestjs/common";
+import { UserService } from "@user/user.service";
 import { Request } from "express";
 
 @Controller()
 export class UserController {
+    constructor(private readonly userService: UserService) {}
+
     @Get("profile")
     public getProfile(@Req() req: Request) {
-        return req.user;
+        return this.userService.findOneById(req.user?.userId ?? "");
     }
 }
