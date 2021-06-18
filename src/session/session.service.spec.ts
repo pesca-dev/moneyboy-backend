257dc66f5c12e742ea0a5db07b769a5c0ae63796
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-06-18 16:30:52 +0200

    Test: Implement first tests for SessionService

---
+++ src/session/session.service.spec.ts
@@ -0,0 +1,78 @@
+import { ISession } from "@interfaces/session";
+import { Session } from "@models/session";
+import { SessionService } from "@session/session.service";
+import { UserService } from "@user/user.service";
+import { Repository } from "typeorm";
+
+const dummySession: ISession = {
+    id: "magicId",
+    createdAt: 123456,
+    user: {
+        id: "randomUserId",
+        username: "dummyUser",
+        displayName: "Dummy User",
+        password: "password",
+        email: "test@example.com",
+        emailVerified: true,
+    },
+};
+
+describe("SessionService", () => {
+    let userServiceMock: UserService;
+    let sessionRepositoryMock: Repository<Session>;
+    let sessionService: SessionService;
+
+    beforeEach(() => {
+        userServiceMock = new (jest.createMockFromModule<any>("@user/user.service").UserService)() as UserService;
+        sessionRepositoryMock = new (jest.createMockFromModule<any>("typeorm").Repository)() as Repository<Session>;
+        sessionService = new SessionService(userServiceMock, sessionRepositoryMock);
+    });
+
+    describe("getSession", () => {
+        beforeEach(() => {
+            jest.spyOn(sessionRepositoryMock, "findOne").mockImplementation(async ({ where: { id } }: any) => {
+                if (id === dummySession.id) {
+                    return dummySession;
+                } else {
+                    return undefined;
+                }
+            });
+        });
+
+        it("shall return undefined, if session is not present", async () => {
+            const session = await sessionService.getSession("someRandomId");
+            expect(session).toBeUndefined();
+        });
+
+        it("shall return session, if id is valid", async () => {
+            const session = await sessionService.getSession(dummySession.id);
+            expect(session).toStrictEqual(dummySession);
+        });
+    });
+
+    describe("destroySession", () => {
+        beforeEach(() => {
+            jest.spyOn(sessionRepositoryMock, "findOne").mockImplementation(async ({ where: { id } }: any) => {
+                if (id === dummySession.id) {
+                    return dummySession;
+                } else {
+                    return undefined;
+                }
+            });
+        });
+
+        it("shall delete a session if id is present and valid", async () => {
+            const mock = jest.fn();
+            jest.spyOn(sessionRepositoryMock, "remove").mockImplementation(async () => mock());
+            await sessionService.destroySession(dummySession.id);
+            expect(mock).toBeCalled();
+        });
+
+        it("shall not delete a session if id is not valid or present", async () => {
+            const mock = jest.fn();
+            jest.spyOn(sessionRepositoryMock, "remove").mockImplementation(async () => mock());
+            await sessionService.destroySession("randomSessionId");
+            expect(mock).toBeCalledTimes(0);
+        });
+    });
+});
