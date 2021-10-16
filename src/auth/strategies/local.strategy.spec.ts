f49fe94244bcd1d2120ec7043c8fde617dad6266
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-10-17 01:58:12 +0200

    Add test for LocalStrategy

---
+++ src/auth/strategies/local.strategy.spec.ts
@@ -0,0 +1,48 @@
+import { AuthService } from "@moneyboy/auth/auth.service";
+import { LocalStrategy } from "@moneyboy/auth/strategies/local.strategy";
+import { UnauthorizedException } from "@nestjs/common";
+
+const dummyData = {
+    username: "testuser",
+    password: "password1234",
+    id: "randomId",
+};
+
+describe("LocalStrategy", () => {
+    let authServiceMock: AuthService;
+    let localStrategy: LocalStrategy;
+
+    beforeEach(() => {
+        authServiceMock = new (jest.createMockFromModule<any>(
+            "@moneyboy/auth/auth.service",
+        ).AuthService)() as AuthService;
+        localStrategy = new LocalStrategy(authServiceMock);
+    });
+
+    describe("validate", () => {
+        beforeEach(() => {
+            jest.spyOn(authServiceMock, "validateUser").mockImplementation(
+                async (username: string, password: string) => {
+                    if (username === dummyData.username && password === dummyData.password) {
+                        return {
+                            id: dummyData.id,
+                        };
+                    }
+                    return null;
+                },
+            );
+        });
+
+        it("shall throw exception, if credentials are wrong", async () => {
+            expect(localStrategy.validate("wrongUser", "wrongPass")).rejects.toThrow(UnauthorizedException);
+            expect(localStrategy.validate(dummyData.username, "wrongPass")).rejects.toThrow(UnauthorizedException);
+            expect(localStrategy.validate("wrongUser", dummyData.password)).rejects.toThrow(UnauthorizedException);
+        });
+
+        it("shall return id, if credentials are wrong", async () => {
+            expect(await localStrategy.validate(dummyData.username, dummyData.password)).toEqual({
+                id: dummyData.id,
+            });
+        });
+    });
+});
