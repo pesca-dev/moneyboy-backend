354a5c5a490f45bf7b10c7298e7b6c720f31646b
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-10-17 18:17:22 +0200

    Add tests for Session

---
+++ src/models/session.spec.ts
@@ -0,0 +1,28 @@
+import { ISession } from "@moneyboy/interfaces/session";
+import { IUser } from "@moneyboy/interfaces/user";
+import { Session } from "@moneyboy/models/session";
+import { hashSync } from "bcrypt";
+import { v4 as uuid } from "uuid";
+describe("Session", () => {
+    it("should be defined", () => {
+        expect(new Session()).toBeDefined();
+    });
+
+    it("should create correct session from data", () => {
+        const dummyUser: IUser = {
+            id: uuid(),
+            displayName: "Display Name",
+            email: "mail@example.com",
+            emailVerified: true,
+            password: hashSync("dummyPassword", 10),
+            username: "dummyUser",
+        };
+
+        const dummySession: ISession = {
+            id: uuid(),
+            createdAt: Date.now(),
+            user: dummyUser,
+        };
+        expect(Session.fromData(dummySession)).toEqual(dummySession);
+    });
+});
